072804a8c78d309c1a721c4e266fe3f44e1ffcbf
tau <36781951+tndd@users.noreply.github.com>
2024-12-09 21:50:32 +0900

    カレンダーにアクセスするためのurl生成機能追加

--- bot/race/generate.py
+++ bot/race/generate.py
@@ -1,10 +1,26 @@
 def gen_year_month() -> list[str]:
+    """
+    各月ごとのカレンダー情報を取得するための年月リスト。
+    """
     ym_list = []
     for year in range(1990, 2025):
         for month in range(1, 13):
             month_str = str(month).zfill(2)
             ym_list.append(str(year) + month_str)
     return ym_list
+
+
+def gen_url_for_access_to_calendar():
+    """
+    このURLからカレンダーの情報を参照。
+    カレンダーからレースがあった日のレースリストページのURLを取得。
+    """
+    urls = []
+    prefix = 'https://db.netkeiba.com/race/list/'
+    for ym in gen_year_month():
+        url = prefix + ym
+        urls.append(url)
+    return urls
 
 
 def test_gen_year_month():
@@ -14,5 +30,13 @@ def test_gen_year_month():
     assert ym_list[-1] == "202412"
 
 
+def test_gen_url_for_access_to_calendar():
+    urls = gen_url_for_access_to_calendar()
+    assert urls[0] == "https://db.netkeiba.com/race/list/199001"
+    assert urls[1] == "https://db.netkeiba.com/race/list/199002"
+    assert urls[-1] == "https://db.netkeiba.com/race/list/202412"
+
+
 if __name__ == "__main__":
     test_gen_year_month()
+    test_gen_url_list_head()
